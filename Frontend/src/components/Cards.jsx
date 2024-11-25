5176376eb5fc94422674579d984a075edeb41986
Muskan <muskansharmajk7@gmail.com>
2024-11-25 19:11:30 +0530

    Frontend Done!!

---
+++ Frontend/src/components/Cards.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+function Cards({item }) {
+  
+  return (
+   <>
+   <div className='mt-4 my-3 p-3'>
+   <div className="card bg-base-100 w-91 shadow-xl hover:scale-105 dark:bg-slate-900 dark-text-white dark:border duration-500 ">
+  <figure>
+    <img
+      src={item.image}
+      alt="Shoes" />
+  </figure>
+  <div className="card-body">
+    <h2 className="card-title">
+      {item.name}
+      <div className="badge badge-secondary">{item.category}</div>
+    </h2>
+    <p>{item.title}</p>
+    <div className="card-actions justify-between">
+      <div className="badge badge-outline">{item.price}</div>
+      <div className=" cursor-pointer px-2 py-1 rounded-full border-[2px]  hover:bg-pink-500  hover:text-white  px-2 py-1 duration-200">Buy Now</div>
+    </div>
+  </div>
+</div></div></>
+  )
+}
+
+export default Cards
